2c35b10da84585aa09cf0bf4291e48888533b145
Dawid Roszman <d.roszman.682@studms.ug.edu.pl>
2022-11-07 10:20:19 +0100

    Dodalem tailwindcss do projektu

---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <h1 className="text-red-300">Hello World </h1>
+    </div>
+  );
+};
+
+export default HelloWorld;

91b4de4d2eb437f5d541681f1d66bc2ef8adf6fb
Dawid Roszman <d.roszman.682@studms.ug.edu.pl>
2022-11-09 20:26:05 +0100

    adjusted colors and added font family

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HelloWorld = () => {
   return (
     <div className='w-100 h-10 bg-secondaryLight dark:bg-secondaryDark flex items-center px-5'>
-        <h1 className='text-primaryLight dark:text-primaryDark font-bold'>Hello World</h1>
+        <h1 className='text-textLight dark:text-textDark font-bold'>Hello World</h1>
     </div>
   )
 }

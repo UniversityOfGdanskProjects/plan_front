284d0f56d5368c2e4227381b8297119b5dc34a47
Dawid Roszman <d.roszman.682@studms.ug.edu.pl>
2022-11-11 17:12:02 +0100

    fixed spelling mistake

--- src/assets/translations/pl.js
+++ src/assets/translations/pl.js
@@ -2,6 +2,6 @@ export const pl_translation = {
     "schedule": "Plan Zajęć",
     "news": "Aktualności",
     "accessibility": "Dostępność",
-    "dark_mode": "Zmiana motywy",
+    "dark_mode": "Zmiana motywu",
     "lang_change": "Zmień język"
 }

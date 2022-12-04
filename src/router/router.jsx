d68aebc5153a906d42fadbe4804faa0098470573
Dawid Roszman <d.roszman.682@studms.ug.edu.pl>
2022-12-04 22:24:06 +0100

    changed router to use components insted of objects

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,23 +1,13 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import App from "../App";
 import Calendar from '../components/Calendar'
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "",
-        element: <Calendar/>
-      },
-      {
-        path: "aktualnosci",
-        element: (<div>Aktualnosci</div>)
-      },
-      {
-        path: "profil",
-        element: (<div> Profil </div>)
-      }
-    ],
-  },
-]);
+import AdminRoute from "../components/admin/AdminRoute";
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="" element={<Calendar />} />
+      <Route path="admin" element={<AdminRoute />} />
+    </Route>
+  )
+);

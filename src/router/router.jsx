42d67ffb12560543cec489ce8f086dc692abf310
Dawid Roszman <d.roszman.682@studms.ug.edu.pl>
2022-12-04 22:25:12 +0100

    removed not existing component from router

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,13 +1,11 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import App from "../App";
 import Calendar from '../components/Calendar'
-import AdminRoute from "../components/admin/AdminRoute";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="" element={<Calendar />} />
-      <Route path="admin" element={<AdminRoute />} />
     </Route>
   )
 );

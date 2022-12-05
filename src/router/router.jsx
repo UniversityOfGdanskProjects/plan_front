774bc1655e0259f85ff50dd8967fcb245b0d6e54
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2022-12-05 19:04:04 +0100

    fixes + add routing to welcomePage, calendar

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,11 +1,17 @@
-import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import App from "../App";
-import Calendar from '../components/Calendar'
+import Calendar from "../components/Calendar";
+import WelcomePage from "../components/WelcomePage";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route path="" element={<Calendar />} />
+      <Route path="" element={<WelcomePage />} />
+      <Route path="kalendarz" element={<Calendar />} />
     </Route>
   )
 );

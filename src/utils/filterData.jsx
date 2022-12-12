8dd65415fb33f083a78b2e1dc3ce0ed2e805f980
Dawid Roszman <d.roszman.682@studms.ug.edu.pl>
2022-12-12 22:33:57 +0100

    added selecting and editing multiple elements

--- src/utils/filterData.jsx
+++ src/utils/filterData.jsx
@@ -1,6 +1,8 @@
-export const filterData = (data, filterBy, Component, handleChange) => {
+export const filterData = (data, filterBy, component_props) => {
       const groups = data.reduce((acc, item) => {
       const group = item[filterBy];
+      
+      
 
       if (!acc[group]) {
         acc[group] = [item];
@@ -13,19 +15,35 @@ export const filterData = (data, filterBy, Component, handleChange) => {
     const groupNames = Object.keys(groups);
     return(
       groupNames.map((groupName, j) => {
+
+        const addGroupToEditable = (group) => {
+          group.forEach((item) => {
+            console.log(item.id);
+            component_props.addToEditable(item.id);
+          });
+        }
+
         const group = groups[groupName];
         const groupElements = group.map((item, i) => (
-          <Component
+          <component_props.Component
             id={item.id}
             i={i}
-            handleChange={handleChange}
+            handleChange={component_props.handleChange}
             key={i}
             schedule={data}
+            addToEditable={component_props.addToEditable}
+            editableItems={component_props.editableItems}
           />
         ));
         return (
           <section key={j} className="mb-4">
-            <h2 className="text-3xl font-bold">{groupName}</h2>
+            <div className="flex justify-between items-center w-fit gap-40">
+              <h2 className="text-3xl font-bold">{groupName}</h2>
+              <div>
+                <label htmlFor={`${j}selectAll`}>Select All</label>
+                <input onChange={() => addGroupToEditable(group)} type="checkbox" className="m-2" id={`${j}selectAll`} name="selectAll"/>
+              </div>
+            </div>
             <div className="grid md:grid-cols-2 lg:grid-cols-3 items-center grid-flow-row gap-4">
               {groupElements}
             </div>
